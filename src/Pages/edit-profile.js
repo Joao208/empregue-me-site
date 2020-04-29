36329bf611722703adb24814cafc803f0d36fd85
João Augusto <augustoj311@gmail.com>
2020-04-29 10:41:34 -0300

    jesh

--- src/Pages/edit-profile.js
+++ src/Pages/edit-profile.js
@@ -98,7 +98,7 @@ const onSelectFile = e => {
       </a>
     </div>
   </nav>
-  <div className="py-4">
+  <form onSubmit={CreateProfile} className="py-4">
     <div className="container">
       <div className="row">
         {/* Main Content */}
@@ -271,13 +271,13 @@ const onSelectFile = e => {
           <div >
           <div className="mb-3 text-right">
             <a className="font-weight-bold btn btn-link rounded p-3" href="/profile"> &nbsp;&nbsp;&nbsp;&nbsp; Cancelar &nbsp;&nbsp;&nbsp;&nbsp; </a>
-            <button onSubmit={CreateProfile} className="font-weight-bold btn btn-primary rounded p-3" style={{color:"white"}}> &nbsp;&nbsp;&nbsp;&nbsp;  Criar perfil &nbsp;&nbsp;&nbsp;&nbsp; </button>
+            <button className="font-weight-bold btn btn-primary rounded p-3" style={{color:"white"}}> &nbsp;&nbsp;&nbsp;&nbsp;  Criar perfil &nbsp;&nbsp;&nbsp;&nbsp; </button>
           </div>
           </div>
         </main>
       </div>
     </div>
-  </div>
+  </form>
 </div>
 {/* Bootstrap core JavaScript */}
 </>
