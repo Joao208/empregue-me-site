595c9b079b9ec834b93b48cd714074978742c76f
João Augusto <augustoj311@gmail.com>
2020-05-05 08:16:20 -0300

    kj

--- src/Pages/jobs.js
+++ src/Pages/jobs.js
@@ -83,27 +83,29 @@ function Feed() {
             </form>
             <div className="tab-content" id="myTabContent">
               <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                <div className="p-3 border-top">
+                {jobs.map(vacancies => (
+                <div key={vacancies._id} className="p-3 border-top">
                   <div className="row">
                     <div className="col-md-6">
                       <a href="job-profile">
                         <div className="border job-item mb-3">
                           <div className="d-flex align-items-center p-3 job-item-header">
                             <div className="overflow-hidden mr-2">
-                              <h6 className="font-weight-bold text-dark mb-0 text-truncate">UI/UX designer</h6>
-                              <div className="text-truncate text-primary">Envato</div>
-                              <div className="small text-gray-500"><i className="feather-map-pin" /> India, Punjab</div>
+                <h6 className="font-weight-bold text-dark mb-0 text-truncate">{vacancies.text.title}</h6>
+                              <div className="text-truncate text-primary">{vacancies.bussines.cnpjI.nome}</div>
+               <div className="small text-gray-500"><a href={`https://www.google.com.br/maps/place/${vacancies.bussines.cnpjI.logradouro}`}><i className="feather-map-pin" />{vacancies.bussines.cnpjI.logradouro}</a></div>
                             </div>
-                            <img className="img-fluid ml-auto" src={img_l1} />
+                            <img className="img-fluid ml-auto" src={vacancies.avatar} />
                           </div>
                           <div className="p-3 job-item-footer">
-                            <small className="text-gray-500"><i className="feather-clock" /> Posted 3 Days ago</small>
+                <small className="text-gray-500"><i className="feather-clock" />{vacancies.createdAt}</small>
                           </div>
                         </div>
                       </a>
                     </div>
                   </div>
                 </div>
+                ))}
               </div>
             </div>
           </div>
