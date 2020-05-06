4910f3a1c2219a6a85864278dfd99a2015575fb7
João Augusto <augustoj311@gmail.com>
2020-05-06 04:20:40 -0300

    kj

--- src/Pages/job-profile.js
+++ src/Pages/job-profile.js
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 
 import '../global.css';
 import '../App.css';
