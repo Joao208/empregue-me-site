ef804492cef297ff26a5be71b9ecd221cadafd8c
João Augusto <augustoj311@gmail.com>
2020-05-07 18:03:28 -0300

    jh

--- src/Pages/job-profile.js
+++ src/Pages/job-profile.js
@@ -159,7 +159,7 @@ const lottieOptionserror = {
                 !! completed 
                && <Lottie options={lottieOptionscompleted
                 } 
-                height='60%'
+                height='100%'
                 width='10%'
                 className="btn btn-primary btn-block text-uppercase"
                 />
