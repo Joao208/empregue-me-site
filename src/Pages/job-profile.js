107f33e1011030c0d589f92f7b4ff3f2eb20c039
João Augusto <augustoj311@gmail.com>
2020-05-07 04:03:54 -0300

    kj

--- src/Pages/job-profile.js
+++ src/Pages/job-profile.js
@@ -81,7 +81,7 @@ function Feed() {
         <div className="col-md-12">
           <div className="d-flex align-items-center py-3">
             <div className="profile-left">
-              <h5 className="font-weight-bold text-dark mb-1 mt-0">{jobs.text}</h5>
+              <h5 className="font-weight-bold text-dark mb-1 mt-0">jobs.text</h5>
               <p className="mb-0 text-muted"><a className="mr-2 font-weight-bold"  href="">jobs.bussines.cnpjI.nome</a> <i className="feather-map-pin" /> Data da postagem</p>
             </div>
             <form className="profile-right ml-auto">
