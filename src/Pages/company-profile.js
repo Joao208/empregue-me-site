657c42e1e029a5a671e515d970b031478cf9b0f8
João Augusto <augustoj311@gmail.com>
2020-05-13 04:12:38 -0300

    j

--- src/Pages/company-profile.js
+++ src/Pages/company-profile.js
@@ -188,7 +188,7 @@ function Feed() {
               <div className="row">
                 <div className="col-md-6">
                   <div className="card overflow-hidden">
-                    <img src={`http://maps.googleapis.com/maps/api/staticmap?center=-22.912869,-43.2289638
+                    <iframe src={`http://maps.googleapis.com/maps/api/staticmap?center=-22.912869,-43.2289638
                     &zoom=15&markers=color:red|-22.909410,-43.2329548&size=250x250`} width="100%" height={150} frameBorder={0} style={{border: 0}} allowFullScreen />
                   </div>
                 </div>
