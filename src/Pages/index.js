001ef000573b4a367997672b8a7117ac1e09b70e
João Augusto <augustoj311@gmail.com>
2020-06-04 15:04:39 -0300

    'a'

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -292,7 +292,7 @@ function Feed() {
             <div className="p-3 border-bottom osahan-post-body">
             </div>
             <iframe 
-            src={`https://maps.google.com/maps?width=100%&amp;height=100%&amp;hl=en&amp;coord=${check.latitude},${check.longitude}&amp;q=1%20Grafton%20Street%2C%20Dublin%2C%20Ireland+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed`} 
+            src={`https://maps.google.com/maps?width=100%&amp;height=100%&amp;hl=en&amp;coord=${check.latitude},${check.longitude}&amp;q=1%20Grafton%20Street%2C%20Dublin%2C%20Ireland+(Empregue.me)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed`} 
             width="100%" 
             height="100%" 
             frameborder="0" 
