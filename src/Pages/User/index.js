86400bd930d6a88073ae240ae50ad0efeb82d3b4
João Augusto <augustoj311@gmail.com>
2020-06-10 15:00:10 -0300

    a

--- src/Pages/User/index.js
+++ src/Pages/User/index.js
@@ -199,21 +199,10 @@ function Feed() {
       <ul className="navbar-nav ml-auto d-flex align-items-center">
       <li className="nav-item dropdown no-arrow d-sm-none">
         <a className="nav-link dropdown-toggle" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <div>
-            <div className="search">
-              <input type="text" className="inputcss" placeholder=" " />
-              <div className="divcss">
-                <svg>
-                  <use xlinkHref="#path">
-                  </use></svg>
-              </div>
-            </div>
-            <svg xmlns="http://www.w3.org/2000/svg" style={{display: 'none'}}>
-              <symbol xmlns="http://www.w3.org/2000/svg" viewBox="0 0 160 28" id="path">
-                <path d="M32.9418651,-20.6880772 C37.9418651,-20.6880772 40.9418651,-16.6880772 40.9418651,-12.6880772 C40.9418651,-8.68807717 37.9418651,-4.68807717 32.9418651,-4.68807717 C27.9418651,-4.68807717 24.9418651,-8.68807717 24.9418651,-12.6880772 C24.9418651,-16.6880772 27.9418651,-20.6880772 32.9418651,-20.6880772 L32.9418651,-29.870624 C32.9418651,-30.3676803 33.3448089,-30.770624 33.8418651,-30.770624 C34.08056,-30.770624 34.3094785,-30.6758029 34.4782612,-30.5070201 L141.371843,76.386562" transform="translate(83.156854, 22.171573) rotate(-225.000000) translate(-83.156854, -22.171573)" />
-              </symbol>
-            </svg>
-          </div>
+        <form className="formd" action="">
+          <input className="inputd" type="search">
+          <i className="fa fa-search"></i>
+        </form>
         </a>
         <div className="dropdown-menu dropdown-menu-right p-3 shadow-sm animated--grow-in" aria-labelledby="searchDropdown">
           <form className="form-inline mr-auto w-100 navbar-search">
