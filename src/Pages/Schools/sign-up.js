0be6672060d037dc283fc1d4c071278230a9d7ac
Joao208 <augustoj311@gmail.com>
2020-06-17 01:31:07 -0300

    Correção avatar cadastro escola

--- src/Pages/Schools/sign-up.js
+++ src/Pages/Schools/sign-up.js
@@ -139,7 +139,7 @@ function Feed() {
                   onChange={event => {
                     setAvatar(event.target.files[0])}
                  }/>
-                 <img src="https://api.adorable.io/avatars/268/abott@adorable" style={{borderRadius:'100%',width:'160px',height:'160px'}} />
+                 <img src="https://api.adorable.io/avatars/268/abott@adorable" style={{borderRadius:'100%',width:'160px',height:'160px',margin:'auto'}} />
             </label>
             <form onSubmit={SignUp}>
               <div className="form-row">
