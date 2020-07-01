55ee0cc92ed539f9eb70f973b3db42b45bc7ba66
Joao208 <augustoj311@gmail.com>
2020-07-01 08:36:29 -0300

    map bussines

--- src/Pages/User/company-profile.js
+++ src/Pages/User/company-profile.js
@@ -221,12 +221,12 @@ async function Unfollow(event){
                 </div>
                 {profile.map(profile => (
                 <MapContainer>
-                  <Map center={[profile.bussines.location.coordinates[0],profile.bussines.location.coordinates[1]]} zoom={15} >
+                  <Map center={[profile.bussines.location.coordinates[1],profile.bussines.location.coordinates[0]]} zoom={15} >
                     <TileLayer
                       attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Marker position={[profile.bussines.location.coordinates[0],profile.bussines.location.coordinates[1]]} />
+                    <Marker position={[profile.bussines.location.coordinates[1],profile.bussines.location.coordinates[0]]} />
                   </Map>
                 </MapContainer>
                 ))}
