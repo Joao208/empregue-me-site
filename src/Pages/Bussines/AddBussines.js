0e067bfe1d819f03582976a1c2b8a28a8f727a6b
Joao <59159025+Joao208@users.noreply.github.com>
2020-07-20 16:40:51 -0300

    anuciar empresa

--- src/Pages/Bussines/AddBussines.js
+++ src/Pages/Bussines/AddBussines.js
@@ -26,7 +26,7 @@ function Feed() {
   
   useEffect(() => {
     async function loadSpots() {
-      const response = await api.get('/profileview')
+      const response = await api.get('/profilebussinesv')
       
       setProfile(response.data.profile)
     }
@@ -68,6 +68,87 @@ function Feed() {
       <div className="row">
         {/* Main Content */}
         <main style={{margin:'auto'}} className="col col-xl-6 order-xl-2 col-lg-12 order-lg-1 col-md-12 col-sm-12 col-12">
+        <form className="box shadow-sm border rounded bg-white mb-3 osahan-share-post" encType="multipart/form-data">
+            <ul className="nav nav-justified border-bottom osahan-line-tab" id="myTab" role="tablist">
+              <li className="nav-item">
+                <a 
+                className="nav-link active" 
+                id="home-tab" 
+                data-toggle="tab" 
+                role="tab" 
+                aria-controls="home" 
+                aria-selected="true"
+                ><i className="feather-edit" />Escrever</a>
+              </li>
+            </ul>
+            <div className="tab-content" id="myTabContent">
+              <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                <div className="p-3 d-flex align-items-center w-100"  href="profile">
+                  {profile.map(profile => (
+                  <div key={profile._id} className="dropdown-list-image mr-3">
+                   <a href="/mycompany-profile"><img className="rounded-circle" src={profile.bussines.avatar} /></a> 
+                    <div className="status-indicator bg-success" />
+                  </div>
+                  ))}
+                  <div className="w-100">
+                    <textarea 
+                    placeholder="Fale sobre sua publicidade..." 
+                    className="form-control border-0 p-0 shadow-none" 
+                    rows={1} 
+                    defaultValue={""} 
+                    value={Text}
+                    onChange={event => setText(event.target.value)}  
+                    />
+                    <textarea 
+                    placeholder="Adicione um link..." 
+                    className="form-control border-0 p-0 shadow-none" 
+                    rows={1} 
+                    defaultValue={""} 
+                    value={Text}
+                    onChange={event => setText(event.target.value)}  
+                    />
+                  </div>
+                </div>
+              </div>
+              {!! avatar 
+                  && <label
+                  id="indexfile"
+                  style={{ backgroundImage: `url(${preview})`}}
+                  className={avatar ? 'has-avatar' : ''}
+                  >
+                  </label>
+                  }
+            </div>
+            <div enc className="border-top p-3 d-flex align-items-center">
+              <form className="mr-auto">
+                <button onClick={
+                  async function checkIn(event){
+                    try{
+                    event.preventDefault()
+                    await api.post('/bussines/check/location')
+                    toast.success('Check-in postado ;)');
+                    }catch(e){
+                    }
+                  }
+                } className="text-link small"
+                style={{border:'none',background:'none'}}
+                >
+                <i className="feather-map-pin" />Check-in</button>
+              </form>
+              <label style={{color:'#000',margin:'auto'}}>
+                <i className="feather-image"></i>
+                  Adicionar Imagem
+                <input style={{display:'none'}} type="file" onChange={event => {
+                      setAvatar(event.target.files[0])}
+                  }/>
+              </label>
+              <div className="flex-shrink-1">
+                <form onSubmit={Post}>
+                  <button className="btn btn-primary btn-block text-uppercase" type="submit" onClick={Post}>{loading ? 'Postando...' : 'Anunciar'}</button>
+                </form>
+              </div>
+            </div>
+          </form>  
         <form className="box shadow-sm border rounded bg-white mb-3 osahan-share-post" encType="multipart/form-data">
             <ul className="nav nav-justified border-bottom osahan-line-tab" id="myTab" role="tablist">
               <li className="nav-item">
