6b2881617e83ef088fe6f14503f30d68097a28d7
Joao <59159025+Joao208@users.noreply.github.com>
2020-07-26 17:53:34 -0300

    jobs

--- src/components/CheckoutPremiun/index.js
+++ src/components/CheckoutPremiun/index.js
@@ -8,39 +8,52 @@ import CardSection from '../Cardsection'
 export default function CheckoutForm() {
     const stripe = useStripe()
     const elements = useElements()
-    async function handleSubmit(event) {
+    const handleSubmit = async (event) => {
+        // We don't want to let default form submission happen here,
+        // which would refresh the page.
         event.preventDefault();
+    ​
         if (!stripe || !elements) {
-            return;
+          // Stripe.js has not yet loaded.
+          // Make sure to disable form submission until Stripe.js has loaded.
+          return;
         }
+    ​
+        // Get a reference to a mounted CardElement. Elements knows how
+        // to find your CardElement because there can only ever be one of
+        // each type of element.
         const cardElement = elements.getElement(CardElement);
+    ​
+        // If a previous payment was attempted, get the latest invoice
         const latestInvoicePaymentIntentStatus = localStorage.getItem(
-            'latestInvoicePaymentIntentStatus'
-        )
-        const {error,paymentMethod} = await stripe.createPaymentMethod({type: 'card',card: cardElement})​
-                if (error) {
-            console.log('[createPaymentMethod error]', error);
+          'latestInvoicePaymentIntentStatus'
+        );
+    ​
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+          type: 'card',
+          card: cardElement,
+        });
+    ​
+        if (error) {
+          console.log('[createPaymentMethod error]', error);
         } else {
-            console.log('[PaymentMethod]', paymentMethod);
-            const paymentMethodId = paymentMethod.id;
-            if (latestInvoicePaymentIntentStatus === 'requires_payment_method') {
-                const invoiceId = localStorage.getItem('latestInvoiceId');
-                retryInvoiceWithNewPaymentMethod({
-                    customerId,
-                    paymentMethodId,
-                    invoiceId,
-                    priceId,
-                });
-            } else {
-                createSubscription({
-                    customerId,
-                    paymentMethodId,
-                    priceId
-                });
-            }
+          console.log('[PaymentMethod]', paymentMethod);
+          const paymentMethodId = paymentMethod.id;
+          if (latestInvoicePaymentIntentStatus === 'requires_payment_method') {
+            // Update the payment method and retry invoice payment
+            const invoiceId = localStorage.getItem('latestInvoiceId');
+            retryInvoiceWithNewPaymentMethod({
+              customerId,
+              paymentMethodId,
+              invoiceId,
+              priceId,
+            });
+          } else {
+            // Create the subscription
+            createSubscription({ customerId, paymentMethodId, priceId });
+          }
         }
-    }
-
+      };
     function createSubscription({
         customerId,
         paymentMethodId,
